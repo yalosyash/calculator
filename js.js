ef6b3609e5e5c56121edb0720833a4099f0a1056
Klepikov Vladislav <yalosyash@yandex.ru>
2023-08-14 16:34:46 +0300

    update js.js

--- js.js
+++ js.js
@@ -1,15 +1,28 @@
 function calculator(string) {
 // let string = "5.4 / gf";
 const expression = string.split(' ');
-const num1 = Math.round(Number(expression[0]));
-const operator = expression[1];
-const num2 = Math.round(Number(expression[2]));
+const a = expression[0];
+const b = expression[1];
+const c = expression[2];
+const romNum = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+const error = 'Throws Error';
 
+if (num1) {}
+
+
+
+
+
+
+//перевели в арабские целые числа
+const num1
+const operator
+const num2
 if(String(num1) === "NaN" || String(num2) === "NaN"){
-    return 'throws Error';
+    return error;
 };
 
-
+console.log(num1);
 let result;
 switch(operator){
     case "+":
@@ -24,14 +37,13 @@ switch(operator){
     case "*":
         result = num1*num2;
     break;
+    default:
+        return error;
 }
-
 result = Math.round(result);
-
 if (result < 1){
     result = 0
 }
-
 
 let answer = String(result);
 console.log(answer);
