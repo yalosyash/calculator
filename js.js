f320aeb855ac1c372af8e6172bd65679ff1fb2a1
Vladislav Klepikov <ya.losyash@yandex.ru>
2023-08-14 22:00:33 +0300

    update js.js

--- js.js
+++ js.js
@@ -1,51 +1,66 @@
 function calculator(string) {
-// let string = "5.4 / gf";
-const expression = string.split(' ');
-const a = expression[0];
-const b = expression[1];
-const c = expression[2];
-const romNum = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
-const error = 'Throws Error';
+    // expect string "a b c" - example "5 + 10"
+    const expression = string.split(' ');
+    const a = expression[0];
+    const b = expression[1];
+    const c = expression[2];
+    const romNum = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+    const error = 'Throws Error';
+    let num1;
+    let num2;
+    let method;
+    // let answer;
+    const ifMethod = () => {
+        switch(b){
+            case "+":
+                method = num1+num2;
+            break;
+            case "-":
+                method = num1-num2;
+            break;
+            case "/":
+                method = num1/num2;
+            break;
+            case "*":
+                method = num1*num2;
+            break;
+            default:
+                console.log("it's not a method")
+                return error;
+        }
+        method < 1 ?  method = 0 : method = Math.floor(method);
+    };
+    const ifArabic = () => {
+        num1 = Math.round(Number(a)); //actions for "a"
+        num2 = Math.round(Number(c)); //actions for "c"
+        if(String(num1) !== "NaN" && String(num2) !== "NaN"){
+            ifMethod();
+            console.log("it's Arabic numbers")
+        } else {
+            console.log("it's not a numbers")
+            return error;
+            }
+    }
+    const ifRome = () => {
+        if (romNum.includes(a) && romNum.includes(c)) {
+            num1 = romNum.indexOf(a)+1; //actions for "a"
+            num2 = romNum.indexOf(c)+1; //actions for "c"
+            ifMethod(); //operation
+            if (method === 0){
+                method = "";
+            }
+            console.log("it's Romanian numbers")
+        } else {
+            ifArabic();
+        }
+    };
+    ifRome();
+    return(String(method));
+}; // end of a main function
+console.log(calculator("5 / 6"));
 
-if (num1) {}
-
-
-
-
-
-
-//перевели в арабские целые числа
-const num1
-const operator
-const num2
-if(String(num1) === "NaN" || String(num2) === "NaN"){
-    return error;
-};
-
-console.log(num1);
-let result;
-switch(operator){
-    case "+":
-        result = num1+num2;
-    break;
-    case "-":
-        result = num1-num2;
-    break;
-    case "/":
-        result = num1/num2;
-    break;
-    case "*":
-        result = num1*num2;
-    break;
-    default:
-        return error;
-}
-result = Math.round(result);
-if (result < 1){
-    result = 0
-}
-
-let answer = String(result);
-console.log(answer);
-};
-console.log(calculator("5.4 / 10"));
+// для проверки
+// числа < 0.5
+// числа > 0.5
+// числа < 1
+// Числа > 1
