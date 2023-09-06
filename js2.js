ed2503d2f0322af5729594b4e63c740ad20e38c9
Vladislav Klepikov <ya.losyash@yandex.ru>
2023-09-06 20:57:47 +0300

    add func

--- js2.js
+++ js2.js
@@ -1,6 +1,6 @@
 const calc = (exp) => {
 	((exp) => {
-		const arrSymbol = [0,1,2,3,4,5,6,7,8,9,'+','-','/','*']
+		const arrSymbol = [0,1,2,3,4,5,6,7,8,9,'+','-','/','*',"(",")"]
 		console.log("good");
 	})(exp);
 	let string = exp;
@@ -10,7 +10,27 @@ const calc = (exp) => {
 	const parseInteg = () => {
 		console.log(str);
 	}
-	const 
+	const isFirst = () => {
+		for (let i = 0; i<str.length; i++){
+			let strNew = "";
+			let n = 1;
+			const notNumber = ["*","/","+","-","(",")"];
+			if (str[i] === "*" || str[i] === "/") {
+				strNew = str[i];
+				do {
+					strNew += str[i+n]
+					n++;
+				} while (!notNumber.includes(str[i+n]))
+				n = -1;
+				do {
+					strNew = str[i+n] + strNew;
+					n--;
+				} while (!notNumber.includes(str[i+n]))
+				str = strNew;
+				replacerStr();
+			}
+		}
+	}
 
 	const replacerStr = () => {
 		parseInteg();
@@ -28,9 +48,12 @@ const calc = (exp) => {
 			}
 		}
 	}
-	isBrackets()? replacerStr():console.log(2)
-
+	!isBrackets() ? replacerStr() : isFirst()
+	console.log(exp)
+	console.log(str)
+	console.log(string)
+	return num;
 };
-console.log(calc("2-5*((5+3)*4)")); //2-5*((5+3)*4)
+console.log(calc("2-5*(51+234*5678+1)")); //2-5*((5+3)*4)
 // a+b*c-d/e+(f-g*(j+k))-i*(x-z)
 // 363636+484884*362662-744774/2626626+(252662-47773*(362662+466363))-277373*(3737-27272)
