b98a4b2a1794dc9f4fb982b98071d6ec2642be67
Klepikov Vladislav <yalosyash@yandex.ru>
2023-09-07 12:27:47 +0300

    udate js2.js

--- js2.js
+++ js2.js
@@ -4,21 +4,43 @@ const calc = (exp) => {
 		const check = exp.search(regSymbol)
 		if (check !== -1) throw new Error("В строке фигня");
 	})();
-
 	let string = exp;
 	let str = "";
 	let num;
-	let operator = ["*","/","+","-"];
-
-	const parseInteg = () => {
-		console.log(str);
-		// str = 
+	let oper;
+	const action = () => {
+		let a = Number(num[0]);
+		let b = Number(num[1]);
+		switch(str[oper]){
+			case "*":
+				num = a*b;
+			break;
+			case "/":
+				num = a/b;
+			break;
+			case "+":
+				num = a+b;
+			break;
+			case "-":
+				num = a-b;
+			break;
+		}
+	}
+	const parseIntegs = () => {
+		num = str.split(str[oper]);
+	}
+	const parseOper = () =>{
+		const regOper = /[+-\/*]/g;
+		oper = str.search(regOper);
 	}
 	const isFirst = () => {
-		const srcExp = str.search(/[*/]/);
+		const srcExp1 = str.search(/[*/]/);
+		const srcExp2 = str.search(/[+-]/);
+		const notNumber = ["*","/","+","-","(",")"];
 		let strNew = "";
 		let n = 1;
-		const notNumber = ["*","/","+","-","(",")"];
+		let srcExp;
+		srcExp1 !==-1 ? srcExp = srcExp1 : srcExp = srcExp2
 		strNew = str[srcExp];
 		do {
 			strNew += str[srcExp+n]
@@ -30,29 +52,58 @@ const calc = (exp) => {
 			n--;
 		} while (!notNumber.includes(str[srcExp+n]))
 		str = strNew;
-		}
-		const replacerStr = () => {
-			parseInteg();
-			string = string.replace(str, num);
-		}
-		const isBrackets = () => {
-			for (let i = string.length - 1; i >= 0; i--) {
-				let n = 0;
-				if (string[i] === "("){
-					do {
-						str += string[i+n];
-						n++;
-					} while (string[i+n-1] !== ")");
-					return str;
-				}
+		parseOper();
+		parseIntegs();
+		action();
+	}
+	const replacerStr = () => {
+		isFirst();
+		string = string.replace(str, num);
+	}
+	// const isBrackets = () => {
+	// 	for (let i = string.length - 1; i >= 0; i--) {
+	// 		let n = 0;
+	// 		if (string[i] === "("){
+	// 			do {
+	// 				str += string[i+n];
+	// 				n++;
+	// 			} while (string[i+n-1] !== ")");
+	// 			return str;
+	// 		}
+	// 	}
+	// }
+	const parseBrackets = (i) => {
+		let n = 0;
+		do {
+			str = string[i+n] + str;
+			n--;
+		} while (string[i+n+1] !== "(");
+	}
+	const isBrackets = () => {
+		const rightBracket = string.search(/[)]/);
+		const leftBracket = string.search(/[(]/);
+		if (rightBracket !== -1 && leftBracket !== -1){
+			parseBrackets(rightBracket)
+			return str;
+		} else return false;
+	}
+	const structure = () => {
+		const regNum = string.search(/[^0-9]/);
+		while (regNum !== -1){
+			if (isBrackets()){
+				replacerStr();
+			} else {
+				str = string;
 			}
+		console.log(exp);
+		console.log(string);
+		console.log(str);
+		console.log(num);
 		}
-		!isBrackets() ? replacerStr() : isFirst()
-		console.log(exp)
-		console.log(string)
-		console.log(str)
-		return num;
-	};
-	console.log(calc("2-5*(51+234*5678)+1")); //2-5*(51+234*5678+1)
-	// a+b*c-d/e+(f-g*(j+k))-i*(x-z)
-	// 363636+484884*362662-744774/2626626+(252662-47773*(362662+466363))-277373*(3737-27272)
+	}
+	structure();
+	return string;
+};
+console.log(calc("2-5*(5+2+2)+1")); //2-5*(51+234*5678+1)
+// a+b*c-d/e+(f-g*(j+k))-i*(x-z)
+// 363636+484884*362662-744774/2626626+(252662-47773*(362662+466363))-277373*(3737-27272)
