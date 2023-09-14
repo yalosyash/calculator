a791a006201933d2574eb7e339e32ef207ef53a7
Klepikov Vladislav <yalosyash@yandex.ru>
2023-09-14 16:49:22 +0300

    update js2.js

--- js2.js
+++ js2.js
@@ -43,35 +43,36 @@ const calc = (exp) => {
 		srcExp1 !==-1 ? srcExp = srcExp1 : srcExp = srcExp2
 		strNew = str[srcExp];
 		do {
-			strNew += str[srcExp+n]
+			strNew += str[srcExp+n];
 			n++;
-		} while (!notNumber.includes(str[srcExp+n]))
+		} while (!notNumber.includes(str[srcExp+n]) && str[srcExp+n] !== str.length -1)
 		n = -1;
 		do {
 			strNew = str[srcExp+n] + strNew;
 			n--;
-		} while (!notNumber.includes(str[srcExp+n]))
+		} while (!notNumber.includes(str[srcExp+n]) && str[srcExp+n] !== str[0])
 		str = strNew;
 		parseOper();
 		parseIntegs();
 		action();
 	}
 	const replacerStr = () => {
+		if(removeBrackets()){
+			string = string.replace(str, str.slice(1, -1));
+			return;
+		}
 		isFirst();
 		string = string.replace(str, num);
+		console.log(str)
+		str = "";
+		num = undefined;
 	}
-	// const isBrackets = () => {
-	// 	for (let i = string.length - 1; i >= 0; i--) {
-	// 		let n = 0;
-	// 		if (string[i] === "("){
-	// 			do {
-	// 				str += string[i+n];
-	// 				n++;
-	// 			} while (string[i+n-1] !== ")");
-	// 			return str;
-	// 		}
-	// 	}
-	// }
+	const removeBrackets = () => {
+		const check = str.slice(1, -1);
+		if (Number(check)) {
+			return true;
+		} return false;
+	};
 	const parseBrackets = (i) => {
 		let n = 0;
 		do {
@@ -80,6 +81,7 @@ const calc = (exp) => {
 		} while (string[i+n+1] !== "(");
 	}
 	const isBrackets = () => {
+		str = ""
 		const rightBracket = string.search(/[)]/);
 		const leftBracket = string.search(/[(]/);
 		if (rightBracket !== -1 && leftBracket !== -1){
@@ -94,6 +96,7 @@ const calc = (exp) => {
 				replacerStr();
 			} else {
 				str = string;
+				replacerStr();
 			}
 		console.log(exp);
 		console.log(string);
@@ -101,9 +104,9 @@ const calc = (exp) => {
 		console.log(num);
 		}
 	}
-	structure();
+	// structure();
 	return string;
 };
-console.log(calc("2-5*(5+2+2)+1")); //2-5*(51+234*5678+1)
+console.log(calc("2+5*(5+2*2)-1")); //2-5*(51+234*5678+1)
 // a+b*c-d/e+(f-g*(j+k))-i*(x-z)
 // 363636+484884*362662-744774/2626626+(252662-47773*(362662+466363))-277373*(3737-27272)
